367c669922902cc70da000db38aa22fddfd156cd
Marcin Jedrzejewski <m.jedrzejewski@collab.pl>
2024-09-19 16:23:53 +0200

    1.0.0 - Refactor

--- src/requireTargetFile.ts
+++
@@ -1,3 +0,0 @@
-// eslint-disable-next-line global-require,import/no-dynamic-require
-const requireTargetFile = (path: string): any => require(path);
-module.exports.requireTargetFile = requireTargetFile;
